46be0386e1c590a5d2fc41af66cd1e212e3397b4
Rachit1313 <Rachitchawla33@gmail.com>
2023-10-31 23:45:27 -0400

    Added search functionality - Fixed #13

--- include/html_generator.py
+++ include/html_generator.py
@@ -14,14 +14,40 @@ def write_html_file(title, content, output_file, stylesheet_url, lang_attribute)
             html_file.write(f"  <link rel='stylesheet' type='text/css' href='{stylesheet_url}'>\n")
         html_file.write("</head>\n")
         html_file.write("<body>\n")
+
+        # Add the search bar HTML form
+        html_file.write("<div style='margin-bottom: 20px;'>")
+        html_file.write("<form id='search-form'>")
+        html_file.write("  <input type='text' id='search-input' placeholder='Search...'>")
+        html_file.write("  <input type='submit' value='Search'>")
+        html_file.write("</form>")
+        html_file.write("</div>")
+
+
         if title:
             html_file.write(f"  <h1>{title}</h1>\n")
+            
         paragraphs = re.split(r'\r?\n\r?\n', content)
 
+        html_file.write("<div id='content'>")
         if content:
             for paragraph in paragraphs:
                 if paragraph.strip():  # Check if the paragraph has content
                     html_file.write(f"  <p>{paragraph}</p>\n")
+        html_file.write("</div>")
+
+                # Add JavaScript code to handle search and highlight functionality
+        html_file.write("<script>")
+        html_file.write("document.getElementById('search-form').addEventListener('submit', function(event) {")
+        html_file.write("  event.preventDefault();")
+        html_file.write("  var searchTerm = document.getElementById('search-input').value;")
+        html_file.write("  var content = document.getElementById('content');")
+        html_file.write("  var regex = new RegExp(searchTerm, 'gi');")
+        html_file.write("  content.innerHTML = content.innerHTML.replace(regex, function(match) {")
+        html_file.write("    return '<span style=\"background-color: yellow;\">' + match + '</span>';")
+        html_file.write("  });")
+        html_file.write("});")
+        html_file.write("</script>")
 
         html_file.write("</body>\n")
         html_file.write("</html>\n")
